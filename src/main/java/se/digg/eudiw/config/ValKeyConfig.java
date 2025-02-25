b2a8dc94d1c49273cc9a69154a9760e235cc4574
Bjorn Molin <extern.bjorn.molin@digg.se>
2025-02-25 22:17:51 +0100

    feat: add valkey/redis as cache - missing files
    Signed-off-by: Bjorn Molin <extern.bjorn.molin@digg.se>

---
+++ src/main/java/se/digg/eudiw/config/ValKeyConfig.java
@@ -0,0 +1,27 @@
+package se.digg.eudiw.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.*;
+import org.springframework.util.MultiValueMap;
+
+import java.time.Duration;
+
+@Configuration
+public class ValKeyConfig {
+    @Bean
+    RedisTemplate<String, MultiValueMap<String, String>> valKeyMultiValueMapTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, MultiValueMap<String, String>> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
+
+
